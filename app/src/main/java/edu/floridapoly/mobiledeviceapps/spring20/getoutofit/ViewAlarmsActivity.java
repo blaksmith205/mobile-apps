cb5b215faa44963b973900b776e4250fde64a4b8
William Blanc <blaksmith57@gmail.com>
2020-03-01 17:18:57 -0500

    Adds basic activities from ui documentation

---
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/spring20/getoutofit/ViewAlarmsActivity.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledeviceapps.spring20.getoutofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAlarmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_alarms);
+    }
+}
