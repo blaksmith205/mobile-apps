d2b88ec3c3466e8f570f50817d376e7a9fc354b7
William Blanc <blaksmith57@gmail.com>
2020-03-01 18:52:36 -0500

    Adds connection to respective activities from MainActivity

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/spring20/getoutofit/MainActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/spring20/getoutofit/MainActivity.java
@@ -2,13 +2,33 @@ package edu.floridapoly.mobiledeviceapps.spring20.getoutofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_INSTANT_MESSAGE= "INSTANT_MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewMessageButton(View view) {
+        Intent intent = new Intent(this, CreateMessageActivity.class);
+        startActivity(intent);
+    }
+
+    public void textAlarmButton(View view) {
+        boolean instantText = false;
+        // Check if the view is the instant text button
+        if (view.getId() == R.id.bt_instant_text)
+            instantText = true;
+
+        Intent intent = new Intent(this, TextAlarmActivity.class);
+        intent.putExtra(EXTRA_INSTANT_MESSAGE, instantText);
+        startActivity(intent);
     }
 }
